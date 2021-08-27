ae03fc06ec6a1ba0e1bff4c7d80c41ffca12316a
Ihor Horb <gorbigorolegovich@gmail.com>
2021-08-27 12:10:04 +0300

    51m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, {useState} from "react";
-import Counter from "./components/Counter";
+import React, {useRef, useState} from "react";
+import './styles/app.css'
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-
+    const [posts, setPosts] = useState([
+        {id:1, title: 'JavaScript', body: 'hello'},
+        {id:2, title: 'Java', body: 'hello2'},
+        {id:3, title: 'Python', body: 'hello3'},
+        {id:4, title: 'Ruby', body: 'hello4'},
+    ])
+    const [title, setTitle] = useState('')
+    const bodyInputRef = useRef()
+    const addNewPost = (e) => {
+      e.preventDefault()
+        console.log(title)
+    }
 
     return (
-        <div className="App">
-            <Counter/>
+        <div className="app">
+            <form>
+                <MyInput value={title}
+                         onChange={(e) => {setTitle(e.currentTarget.value)}}
+                         type="text"
+                         placeholder={'Название поста'}/>
+                <MyInput ref={bodyInputRef}
+                    type="text"
+                         placeholder={'описание поста'}/>
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title={'Список постов'}/>
         </div>
     );
 }
--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-const ClassComponent = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+class ClassComponent extends React.Component {
 
-export default ClassComponent;
+    constructor(props) {
+        super(props);
+        this.state = {
+            count:0
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+    }
+
+     increment = () => {
+        this.setState({count: this.state.count + 1})
+    }
+     decrement = () => {
+         this.setState({count: this.state.count - 1})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.increment}>inc</button>
+                <button onClick={this.decrement}>dec</button>
+            </div>
+        )
+    }
+}
+
+export default ClassComponent
